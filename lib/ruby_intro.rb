fe13d74f07c931e93cb2e9388a726d15619e4005
Sahassawat <i.morkey@msn.com>
2022-08-01 23:19:27 +0700

    part 1 and part 2 done !!!

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -19,13 +19,13 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  if arr.empty? || arr.length <= 1 # Check if the array is empty or has only one element
+  if arr.empty? || arr.length <= 1 # Check if the given array is empty or has only one element
     return false
   else
-    if !arr.combination(2).find(proc {false}) {|x,y| x + y == n}
-      return false
-    else
-      return true
+    if !arr.combination(2).find(proc {false}) {|x,y| x + y == n} # .combination to get all pairs of elements in the array
+      return false                                               # then find with .find method
+    else                                                         # a = [1, 2, 3, 4]  
+      return true                                                # a.combination(2).to_a  => [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
     end
   end
 end
@@ -33,15 +33,24 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s == "" || s.downcase.start_with?(/[^a-z]/) # Check if the  given string is empty or didn't start with a-z(nonletters)
+    return false
+  else
+    return !s.downcase.start_with?(/[aeiou]/) # Check if the given string start with consonant (a, e, i, o, u)
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  binary = /^[1|0]+/.match(s)
+  if binary 
+    return (binary.to_s.to_i(2) % 4 == 0)
+  else
+    return false
+  end
 end
 
 # Part 3
@@ -49,3 +58,6 @@ end
 class BookInStock
 # YOUR CODE HERE
 end
+
+puts binary_multiple_of_4?('')
+
